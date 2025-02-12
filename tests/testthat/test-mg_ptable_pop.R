1d3734f4394e9fc45d93f67d79a571a0fb54b0ca
Shu Fai Cheung <shufai.cheung@gmail.com>
2025-02-12 08:41:32 +0800

    Update test-mg_ptable_pop.R

--- tests/testthat/test-mg_ptable_pop.R
+++ tests/testthat/test-mg_ptable_pop.R
@@ -20,3 +20,22 @@ out <- ptable_pop(model = model1,
                   pop_es = model1_es)
 mm_out <- model_matrices_pop(out)
 mm_lm_out <- mm_lm(mm_out)
+
+
+model2 <-
+"
+m ~ x
+y ~  m + x
+"
+
+model2_es <- list("m ~ x" = "-m",
+                  "y ~ m" = "l",
+                  "y ~ x" = c("m", "l", "n"))
+
+
+data_i <- sim_data_i(model = model2,
+                     pop_es = model2_es,
+                     n = 200,
+                     number_of_indicators = c(x = 3, y = 4, m = 5),
+                     reliability = c(x = .60, y = .70, m = .80),
+                     seed = 1234)
