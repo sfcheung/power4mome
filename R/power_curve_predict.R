051e47ee8c65b89ca0030cd8ffd9a54ca0c07428
Shu Fai Cheung <shufai.cheung@gmail.com>
2025-05-18 01:31:17 +0800

    Update the doc of predict.power_curve()
    Tests and checks passed.

--- R/power_curve_predict.R
+++ R/power_curve_predict.R
@@ -5,8 +5,21 @@
 #' [power_curve()].
 #'
 #' @details
-#' It retrieve the stored results
-#' and call the corresponding methods.
+#' It works in two modes. If new
+#' data is not supplied (through
+#' `newdata`), it retrieve the stored
+#' results and call the corresponding
+#' methods to compute the predicted
+#' values, which are the predicted
+#' rejection rates (power levels if
+#' the population effect size is
+#' equal to zero).
+#'
+#' If new data is supplied, such as
+#' the named list with a vector of
+#' sample sizes, they will be used to
+#' compute the predicted rejection
+#' rates.
 #'
 #' @return
 #' It returns a numeric vector of
@@ -16,8 +29,11 @@
 #' object.
 #'
 #' @param newdata A data frame with
-#' a column named `x`. This is a
-#' required argument.
+#' a column named `x`. It can also
+#' be a named list, with one element
+#' named `x` and is a vector of the
+#' values. If not supplied, values of
+#' `x` stored in `object` will be used.
 #'
 #' @param ...  Additional arguments.
 #' Passed to the corresponding
@@ -26,9 +42,59 @@
 #' @seealso [power_curve()].
 #'
 #' @examples
-#' # TODO:
-#' # - To write
-#' x <- 1
+#'
+#' model_simple_med <-
+#' "
+#' m ~ x
+#' y ~ m + x
+#' "
+#'
+#' model_simple_med_es <- c("y ~ m" = "l",
+#'                          "m ~ x" = "m",
+#'                          "y ~ x" = "s")
+#'
+#' sim_only <- power4test(nrep = 10,
+#'                        model = model_simple_med,
+#'                        pop_es = model_simple_med_es,
+#'                        n = 50,
+#'                        fit_model_args = list(fit_function = "lm"),
+#'                        do_the_test = FALSE,
+#'                        iseed = 1234,
+#'                        parallel = FALSE,
+#'                        progress = FALSE)
+#'
+#' # By n
+#'
+#' out1 <- power4test_by_n(sim_only,
+#'                         nrep = 10,
+#'                         test_fun = test_parameters,
+#'                         test_args = list(par = "y~x"),
+#'                         n = c(25, 100, 200, 1000),
+#'                         by_seed = 1234,
+#'                         parallel = FALSE,
+#'                         progress = FALSE)
+#'
+#' pout1 <- power_curve(out1)
+#' pout1
+#' predict(pout1,
+#'         newdata = list(x = c(150, 250, 500)))
+#'
+#' # By pop_es
+#'
+#' out2 <- power4test_by_pop_es(sim_only,
+#'                              nrep = 10,
+#'                              test_fun = test_parameters,
+#'                              test_args = list(par = "y~x"),
+#'                              pop_es_name = "y ~ x",
+#'                              pop_es_values = seq(0, .7, .15),
+#'                              by_seed = 1234,
+#'                              parallel = FALSE,
+#'                              progress = FALSE)
+#'
+#' pout2 <- power_curve(out2)
+#' pout2
+#' predict(pout2,
+#'         newdata = list(x = c(.25, .55)))
 #'
 #' @export
 
