8255fb820c8e52cfab59a0a9257065d68f9d9fa8
Shu Fai Cheung <shufai.cheung@gmail.com>
2025-06-11 03:25:12 +0800

    Add a few more tests

--- tests/testthat/test-x_from_power_algorithm_bisection_difficult_cases.R
+++ tests/testthat/test-x_from_power_algorithm_bisection_difficult_cases.R
@@ -80,4 +80,65 @@ out_by_es_ab <- power4test_by_es(out,
                                  pop_es_values = seq(.05, .15, .01))
 rejection_rates(out_by_es_ab)
 
+# Close enough
+
+
+mod <-
+"
+m ~ x
+y ~ m + x
+"
+
+mod_es <-
+"
+m ~ x: m
+y ~ m: l
+y ~ x: nil
+"
+
+out <- power4test(nrep = 2,
+                  model = mod,
+                  pop_es = mod_es,
+                  n = 50000,
+                  iseed = 1234)
+
+out <- power4test(nrep = 100,
+                  model = mod,
+                  pop_es = mod_es,
+                  n = 50,
+                  R = 1000,
+                  ci_type = "mc",
+                  test_fun = test_indirect_effect,
+                  test_args = list(x = "x",
+                                   m = "m",
+                                   y = "y",
+                                   mc_ci = TRUE),
+                  iseed = 1234,
+                  parallel = TRUE)
+
+
+out_es_a <- x_from_power(out,
+                         x = "es",
+                         pop_es_name = "m~x",
+                         target_power = .80,
+                         final_nrep = 400,
+                         seed = 4567,
+                         what = "ub",
+                         goal = "close_enough")
+
+summary(out_es_a)
+plot(out_es_a)
+
+out_es_b <- x_from_power(out,
+                         x = "es",
+                         pop_es_name = "m~x",
+                         target_power = .80,
+                         final_nrep = 400,
+                         seed = 4567,
+                         what = "lb",
+                         goal = "close_enough")
+
+summary(out_es_b)
+plot(out_es_b)
+
 })
