0f57be294c5c1015da0c7aaffc47276d64f4d0cb
Shu Fai Cheung <shufai.cheung@gmail.com>
2025-07-21 19:47:53 +0800

    0.0.1.93: Add omnibus to test_parameters()
    Tests and checks passed.

---
+++ tests/testthat/test-test_all_pars_lm_omnibus.R
@@ -0,0 +1,80 @@
+library(testthat)
+
+test_that("All parameters: lm", {
+
+model_simple_med <-
+"
+m ~ x
+y ~ m + x
+"
+
+model_simple_med_es <- c("y ~ m" = "l",
+                         "m ~ x" = "m",
+                         "y ~ x" = "s")
+
+sim_only <- power4test(nrep = 10,
+                       model = model_simple_med,
+                       pop_es = model_simple_med_es,
+                       n = 50,
+                       fit_model_args = list(fit_function = "lm"),
+                       do_the_test = FALSE,
+                       iseed = 1234,
+                       progress = FALSE)
+
+test_out <- power4test(object = sim_only,
+                       test_fun = test_parameters,
+                       progress = FALSE)
+
+chk0 <- rejection_rates(test_out, collapse = "all_sig")
+chk1 <- rejection_rates(test_out, collapse = "at_least_one_sig")
+chk2 <- rejection_rates(test_out, collapse = "at_least_k_sig", at_least_k = 1)
+chk3 <- rejection_rates(test_out, collapse = "at_least_k_sig", at_least_k = 2)
+chk4 <- rejection_rates(test_out, collapse = "at_least_k_sig", at_least_k = 3)
+
+test_out <- power4test(object = sim_only,
+                       test_fun = test_parameters,
+                       test_args = list(omnibus = "all_sig"),
+                       progress = FALSE)
+(chk <- test_summary(test_out))
+expect_equal(chk0$reject,
+             chk[[1]]$sig)
+
+test_out <- power4test(object = sim_only,
+                       test_fun = test_parameters,
+                       test_args = list(omnibus = "at_least_one_sig"),
+                       progress = FALSE)
+(chk <- test_summary(test_out))
+expect_equal(chk1$reject,
+             chk[[1]]$sig)
+
+test_out <- power4test(object = sim_only,
+                       test_fun = test_parameters,
+                       test_args = list(omnibus = "at_least_k_sig",
+                                        at_least_k = 1),
+                       progress = FALSE)
+(chk <- test_summary(test_out))
+expect_equal(chk1$reject,
+             chk[[1]]$sig)
+
+test_out <- power4test(object = sim_only,
+                       test_fun = test_parameters,
+                       test_args = list(omnibus = "at_least_k_sig",
+                                        at_least_k = 2),
+                       progress = FALSE)
+(chk <- test_summary(test_out))
+expect_equal(chk3$reject,
+             chk[[1]]$sig)
+
+test_out <- power4test(object = sim_only,
+                       test_fun = test_parameters,
+                       test_args = list(omnibus = "at_least_k_sig",
+                                        at_least_k = 3),
+                       progress = FALSE)
+(chk <- test_summary(test_out))
+expect_equal(chk4$reject,
+             chk[[1]]$sig)
+expect_equal(chk0$reject,
+             chk[[1]]$sig)
+
+
+})
