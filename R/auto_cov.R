099fd20239c9ff93e23c7dbb0d6f0b4a15bf01ec
Shu Fai Cheung <shufai.cheung@gmail.com>
2025-08-26 21:50:50 +0800

    WIP

--- R/auto_cov.R
+++ R/auto_cov.R
@@ -1,197 +1,210 @@
-# From semhelpinghands
-# Modified for internal use
-
-#' @title Add Covariances Between
-#' Exogenous Variables
-#'
-#' @description It generates the
-#' 'lavaan' model syntax for exogenous
-#' variables in a `lavaan` model.
-#'
-#' @details The function [lavaan::sem()]
-#' usually will set covariances between
-#' "exogenous" variables free when
-#' `fixed.x = FALSE` ("exogenous" is
-#' defined here as variables that appear
-#' on the right hand side but not on the
-#' left hand side of the `~` operator`).
-#' However, if a covariance between the
-#' residual term of an endogenous
-#' variable and an exogenous variable is
-#' manually set to free, [lavaan::sem()]
-#' may not set the aforementioned
-#'  covariances free. Users will need to
-#' free them manually, and there may be
-#' a lot of them in some models.
-#'
-#' This function gets a model syntax
-#' and generates the syntax for these
-#' covariances. Users can then inspect
-#' it, modify it if necessary, and then
-#' copy and paste it to the model
-#' syntax.
-#'
-#' @return
-#' [add_exo_cov()] returns a one-element
-#' character vector of the syntax, with
-#' lines separated by "\\n". The
-#' generated syntax is appended to the
-#' input model syntax.
-#'
-#' [auto_exo_cov()] returns a
-#' one-element character vector of the
-#' generated syntax, with lines
-#' separated by "\\n".
-#'
-#' @param model The model syntax to
-#' which the covariances are to be
-#' added.
-#'
-#' @param FUN Name (as string) of the
-#' `lavaan` wrapper to be called.
-#' Normally should be `"sem"`, the
-#' default.
-#'
-#' @param print Logical. Whether the
-#' generated syntax should also be
-#' printed by [cat()]. Default is
-#' `TRUE`.
-#'
-#' @author Shu Fai Cheung
-#' <https://orcid.org/0000-0002-9871-9448>
-#'
-#' @examples
-#'
-#' library(lavaan)
-#' set.seed(8976223)
-#' n <- 100
-#' x <- rnorm(n)
-#' m <- .5 * x + rnorm(n, 0, sqrt(.4))
-#' z <- rnorm(n)
-#' y <- .4 * m + .3 * z * m + rnorm(n, 0, .5)
-#' dat <- data.frame(x, m, z, y)
-#' dat$zm <- dat$z * dat$m
-#' mod0 <-
-#' "
-#' m ~ x
-#' y ~ m + z + zm
-#' m ~~ z + zm
-#' "
-#' fit <- sem(mod0, dat, fixed.x = FALSE)
-#'
-#' # Add covariances. Also printed by default.
-#' mod0_cov <- add_exo_cov(mod0)
-#'
-#' # Fit the model
-#' fit_cov <- sem(mod0_cov, dat, fixed.x = FALSE)
-#'
-#' # Manually adding the covariances
-#' mod1 <-
-#' "
-#' m ~ x
-#' y ~ m + z + zm
-#' m ~~ z + zm
-#' z  ~~ zm + x
-#' zm ~~ x
-#' "
-#' fit1 <- sem(mod1, dat, meanstructure = TRUE, fixed.x = FALSE)
-#'
-#' # Compare the results
-#'
-#' # No manual covariances
-#' fit
-#'
-#' # Automatically generated covariances
-#' fit_cov
-#'
-#' # Manually added covariances
-#' fit1
-#'
 #' @noRd
-
-#' @describeIn auto_cov Add covariances
-#' between exogenous variables to the
-#' model syntax and than return the
-#' modified model syntax.
-#' @noRd
-add_exo_cov <- function(model, FUN = "sem", print = TRUE) {
-  out <- paste0(model, "\n",
-                "# Added by auto_exo_cov\n",
-                auto_exo_cov(model = model,
-                              FUN = FUN,
-                              print = FALSE))
-  if (print) {
-    cat(out)
-  }
-  out
+# Input:
+# - Parameter table
+# - Original model syntax
+# Output:
+# -
+pt_with_int <- function(ptable,
+                        model_original) {
+  to_add <- cov_to_add(ptable)
+  # TODO:
+  # - When `x:w ~~ y:z` is available in
+  #   lavaan, return the model syntax directly.
+  model_fixed <- paste(
+                  c(model_original,
+                    to_add,
+                    "\n"),
+                  collapse = "\n")
+  model_fixed_2 <- gsub(
+                     ":",
+                     "__xx__",
+                     model_fixed,
+                     fixed = TRUE)
+  fit_fixed <- fake_fit_for_int(
+                  ptable = ptable,
+                  model_int = model_fixed_2)
+  pt_fixed <- lavaan::parameterTable(fit_fixed$fit)
+  pt_fixed$lhs <- gsub("__xx__",
+                       ":",
+                       pt_fixed$lhs,
+                       fixed = TRUE)
+  pt_fixed$rhs <- gsub("__xx__",
+                       ":",
+                       pt_fixed$rhs,
+                       fixed = TRUE)
+  pt_fixed <- merge_start(pt_source = ptable,
+                          pt_target = pt_fixed)
+  pt_fixed
 }
 
-#' @describeIn auto_cov Generate the
-#' model syntax for the covariances
-#' between exogenous variables.
 #' @noRd
-auto_exo_cov <- function(model, FUN = "sem", print = TRUE) {
-  # TODO:
-  # - Is requireNamespace() necessary?
-  requireNamespace("lavaan", quietly = TRUE)
-  fit0 <- do.call(FUN,
-                  list(model = model,
-                        do.fit = FALSE,
-                        fixed.x = TRUE,
-                        warn = FALSE))
-  # TODO:
-  # - Support multigroup models
-  if (lavaan::lavInspect(fit0, "ngroups") != 1) {
-    stop("Does not support a model with more than one group.")
-  }
-  isivov <- get_exo(fit0, type = "ov")
-  isivlv <- get_exo(fit0, type = "lv")
-  if (length(isivov) > 1) {
-    outov <- gen_cov(isivov)
+# Input:
+# - A parameter table or a lavaan object
+# Output:
+# - A vector of covariances to be added
+cov_to_add <- function(object) {
+  # Also works for a multigroup model
+  if (inherits(object, "lavaan")) {
+    pt <- lavaan::parameterTable(object)
   } else {
-    outov <- character(0)
+    pt <- object
   }
-  if (length(isivlv) > 1) {
-    outiv <- gen_cov(isivlv)
-  } else {
-    outiv <- character(0)
+  ov_x <- lavaan::lavNames(pt, "ov.x")
+  ov_int <- lavaan::lavNames(pt, "ov.interaction")
+  ov_nox <- lavaan::lavNames(pt, "ov.nox")
+  ov_nox <- setdiff(ov_nox, ov_int)
+  cov_add <- list()
+  for (xx in ov_int) {
+    ww <- strsplit(xx, ":", fixed = TRUE)[[1]]
+    tmp <- combn(ww, m = 2, simplify = TRUE)
+    cov_add <- c(cov_add,
+                mapply(function(lhs, rhs) {
+                  data.frame(lhs = lhs,
+                             op = "~~",
+                             rhs = rhs)
+                },
+                lhs = c(ww, tmp[1, ]),
+                rhs = c(rep(xx, length(ww)),
+                        tmp[2, ]),
+                SIMPLIFY = FALSE,
+                USE.NAMES = FALSE))
   }
-  out <- paste0(outov, outiv)
-  if (print) {
-    cat(out)
-  }
-  out
-}
-
-# Get the exogenous observed variables and latent variables
-#' @noRd
-
-get_exo <- function(fit, type = c("ov", "lv")) {
-  # TODO:
-  # - Use lavaan::lavNames() to find these variables
-  type <- match.arg(type)
-  ptable <- lavaan::parameterTable(fit)
-  isdv <- unique(ptable$lhs[ptable$op == "~"])
-  onrhs <- unique(ptable$rhs[ptable$op == "~"])
-  isiv <- setdiff(onrhs, isdv)
-  isiv[isiv %in% lavaan::lavNames(fit, type)]
-}
-
-# Generate covariances
-#' @noRd
-
-gen_cov <- function(vars) {
-  p <- length(vars)
-  out <- character(0)
-  for (i in seq_len(p)) {
-    if (i < p) {
-      out <- paste0(out,
-                    vars[i],
-                    " ~~ ",
-                    paste0(vars[-seq_len(i)],
-                          collapse = " + "),
-                    "\n")
+  cov_add <- do.call(rbind,
+                     cov_add)
+  i_w <- cov_add$lhs %in% ov_x
+  if (any(i_w)) {
+    cov_add2 <- list()
+    for (xx in unique(cov_add$rhs[i_w])) {
+      cov_add2 <- c(cov_add2,
+                  mapply(function(lhs, rhs) {
+                    data.frame(lhs = lhs,
+                               op = "~~",
+                               rhs = rhs)
+                  },
+                  lhs = ov_x,
+                  rhs = xx,
+                  SIMPLIFY = FALSE,
+                  USE.NAMES = FALSE))
     }
+    cov_add <- rbind(cov_add,
+                     do.call(rbind, cov_add2))
   }
-  out
+  cov_add <- unique(cov_add)
+  pt_cov <- pt[(pt$op == "~~") &
+               (pt$free > 0) &
+               (pt$lhs != pt$rhs) &
+               ((pt$lhs %in% ov_x) & (pt$rhs %in% ov_x)), ]
+  i1 <- grepl(":", pt_cov$lhs)
+  tmp1 <- pt_cov[i1, "lhs"]
+  pt_cov[i1, "lhs"] <- pt_cov[i1, "rhs"]
+  pt_cov[i1, "rhs"] <- tmp1
+  pt_cov1 <- pt_cov[, c("lhs", "op", "rhs")]
+  pt_cov_all <- rbind(cov_add,
+                      pt_cov1)
+  pt_cov_all <- unique(pt_cov_all)
+  i <- pt_cov_all$lhs != pt_cov_all$rhs
+  pt_cov_all <- pt_cov_all[i, ]
+  # Remove duplicated covariances
+  for (i in seq_len(nrow(pt_cov_all))) {
+    tmp_i <- unlist(pt_cov_all[i, c("lhs", "rhs")])
+    tmp2 <- order(tmp_i)
+    pt_cov_all[i, c("lhs", "rhs")] <- unname(tmp_i[tmp2])
+  }
+  pt_cov_all <- pt_cov_all[!duplicated(pt_cov_all), , drop = FALSE]
+  pt_cov_all <- apply(pt_cov_all,
+                      MARGIN = 1,
+                      paste,
+                      collapse = " ")
+  pt_cov_all <- unname(pt_cov_all)
+  pt_cov_all
+}
+
+#' @noRd
+# Input:
+# - A parameter table.
+# - A model syntax.
+# Output:
+# - A list:
+#   - A fitted object
+#   - A list of fake covariance matrices
+#   - A list of fake mean vectors
+fake_fit_for_int <- function(
+                        ptable,
+                        model_int
+                      ) {
+  # Multigroup models supported
+  ngroups <- max(ptable$group)
+  gpnames <- paste0("gp", seq_len(ngroups))
+  fit0 <- lavaan::sem(model_int,
+                      do.fit = FALSE)
+  vnames <- lavaan::lavNames(fit0,
+                             type = "ov")
+  p <- length(vnames)
+  d1 <- diag(p)
+  colnames(d1) <- rownames(d1) <- vnames
+  m1 <- vector(mode = "numeric",
+               length = p)
+  names(m1) <- vnames
+  dat_cov <- lapply(seq_len(ngroups),
+                    function(x) d1)
+  dat_means <- lapply(seq_len(ngroups),
+                     function(x) m1)
+  fit1 <- lavaan::sem(model_int,
+                      sample.cov = dat_cov,
+                      sample.mean = dat_means,
+                      sample.nobs = rep(10000, ngroups),
+                      do.fit = FALSE,
+                      group.label = gpnames,
+                      meanstructure = (ngroups > 1),
+                      fixed.x = FALSE)
+  list(fit = fit1,
+       cov = dat_cov,
+       mean = dat_means)
+}
+
+#' @noRd
+# Input:
+# - A parameter table.
+# Output:
+# - A parameter table, with covariance terms
+#   ordered.
+order_cov <- function(pt) {
+  for (i in seq_len(nrow(pt))) {
+    if (pt[i, "op", drop = TRUE] != "~~") next
+    tmp_i <- unlist(pt[i, c("lhs", "rhs")])
+    tmp2 <- order(tmp_i)
+    pt[i, c("lhs", "rhs")] <- unname(tmp_i[tmp2])
+  }
+  pt
+}
+
+#' @noRd
+# Input:
+# - The source parameter table with starting values to be imported
+# - The target parameter table.
+# Output:
+# - The target parameter table with starting values updated.
+merge_start <- function(pt_source,
+                        pt_target) {
+  if (is.null(pt_source$group)) {
+    ngroups <- 1
+  } else {
+    ngroups <- max(pt_source$group)
+  }
+  pt_source <- order_cov(pt_source)
+  pt_target <- order_cov(pt_target)
+  pt_source$tmplabel <- lavaan::lav_partable_labels(pt_source)
+  pt_target$tmplabel <- lavaan::lav_partable_labels(pt_target)
+  pt_target$tmpid <- seq_len(nrow(pt_target))
+  col0 <- c("lhs", "op", "rhs", "tmplabel")
+  tmp <- merge(pt_target[, c(col0, "tmpid")],
+               pt_source[, c(col0, "start")],
+               all.x = TRUE,
+               sort = FALSE)
+  tmp <- tmp[order(tmp$tmpid), ]
+  i <- !is.na(tmp$start)
+  pt_target[i, "start"] <- tmp[i, "start"]
+  pt_target$tmplabel <- NULL
+  pt_target$tmpid <- NULL
+  pt_target
 }
